4375cf10e9f3b5328f7f91bd636eccf35d27c480
ljcp28ljcp <ljcp28ljcp@99eec449-2851-0410-a4d2-bb893c3c0773>
2008-08-07 20:49:53 +0000

    lab

---
+++ trunk/MotoR1/bo/FacturaBo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bo;
+
+import model.Factura;
+/**
+ *
+ * @author luisj
+ */
+public interface FacturaBo {
+    
+    public double save(Factura fact);
+    
+    
+}
