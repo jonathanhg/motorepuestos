02541592ef7cb47071fc5ae947b2f5a15addcf25
jonathanhg <jonathanhg@99eec449-2851-0410-a4d2-bb893c3c0773>
2008-08-17 21:03:49 +0000

    Casi terminado el modulo Gerencial: se realizo=> cantidad ventas por producto en el mes. Falta: cantidad de Rotacion por Producto;

--- trunk/MotoR1/Reportes/ModuloGerencial.java
+++ trunk/MotoR1/Reportes/ModuloGerencial.java
@@ -2,14 +2,171 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Reportes;
+
+import daoHibernateImpl.FacturaDaoImpl;
+import daoHibernateImpl.ProductoDaoImpl;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import model.FactProduct;
+import model.Factura;
+import model.Producto;
+
+import java.io.*;
+
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import jxl.*;
+import jxl.write.*;
+import model.Producto;
 
 /**
  *
  * @author Administrador
  */
+class ProductoX {
+
+    private String nombre;
+    private String descripcion;
+    private int cantVendidas;
+    private int cantRotaciones;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantVendidas() {
+        return cantVendidas;
+    }
+
+    public void setCantVendidas(int cantidad) {
+        this.cantVendidas = cantidad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getCantRotaciones() {
+        return cantRotaciones;
+    }
+
+    public void setCantRotaciones(int cantRotaciones) {
+        this.cantRotaciones = cantRotaciones;
+    }
+}
+
 public class ModuloGerencial {
-public void canditadVentasProducto(){} //producto con mayor o menor cantidad de ventas
-public void cantidadRotacionProducto(){} //productos con mayor o menor rotación
+
+    //producto con mayor o menor cantidad de ventas
+    public void canditadVentasProducto(int mes) {
+        List<ProductoX> listaProdX = new ArrayList<ProductoX>();
+        List<ProductoX> listaContadaProdX = new ArrayList<ProductoX>();
+        FacturaDaoImpl factManager = new FacturaDaoImpl();
+
+        //obtenemos todas las facturas del mes indicado
+        List facturas = factManager.facturasPorMes(mes);
+        Iterator itFacturas = facturas.iterator();
+
+        //llenamos a listaProdX con los productos vendidos en el mes indicado
+        Factura factTemp;
+        while (itFacturas.hasNext()) {
+            factTemp = (Factura) itFacturas.next();
+            ProductoX producto = new ProductoX();
+            FactProduct facProd = (FactProduct) factTemp.getProductos();
+            producto.setNombre(facProd.getId());
+            producto.setDescripcion(facProd.getDescripcion());
+            producto.setCantVendidas(producto.getCantVendidas() + 1);
+            listaProdX.add(producto);
+        }
+
+        ProductoDaoImpl prodManager = new ProductoDaoImpl();
+
+        //llenamos a prodOriginales con todos los productos del inventario
+        List prodOriginales = prodManager.obtenerProductos();
+        Iterator itProdOriginales = prodOriginales.iterator();
+        Producto prodTemp;
+        while (itProdOriginales.hasNext()) {
+            ProductoX producto = new ProductoX();
+            prodTemp = (Producto) itProdOriginales.next();
+            producto.setNombre(prodTemp.getId());
+            producto.setDescripcion(prodTemp.getDescripcion());
+            producto.setCantVendidas(this.vecesRepetido(listaProdX, prodTemp.getId()));
+            listaContadaProdX.add(producto);
+        }
+        crearExcel(listaContadaProdX.iterator(), "CantidadVentasProducto.xls", "CantidadVentasProducto", "PRODUCTOS VENDIDOS EN EL MES: " + mes + "DEL AÑO "+ (new Date().getYear())+1900);
+
+    }
+
+//productos con mayor o menor rotación
+    public void cantidadRotacionProducto() {
+    }
+
+    //retorna el número de veces que se repite un codigo en la lista
+    private int vecesRepetido(List<ProductoX> listaProdX, String nombreProducto) {
+        int result = 0;
+        for (int i = 0; i < listaProdX.size(); i++) {
+            if (listaProdX.get(i).getNombre().equals(nombreProducto)) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    private void crearExcel(Iterator itProd, String nombreArchivo, String nombreHoja, String nombreReporte) {
+        ProductoX prodTemp;
+
+        try {
+            //Se crea el libro Excel
+            WritableWorkbook workbook = Workbook.createWorkbook(new File(nombreArchivo));
+
+            //Se crea una nueva hoja dentro del libro
+            WritableSheet sheet = workbook.createSheet(nombreHoja, 0);
+
+            //Creamos celdas de los titulos
+            sheet.addCell(new jxl.write.Label(2, 0, nombreReporte)); //(columna+1) & (fila+1) & (dato)
+
+            sheet.addCell(new jxl.write.Label(0, 3, "CODIGO DE PRODUCTO"));
+            sheet.addCell(new jxl.write.Label(1, 3, "DESCRIPCION"));
+            sheet.addCell(new jxl.write.Label(2, 3, "CANTIDAD VENDIDA")); //minimo
+
+            int fila = 4;
+            for (int i = 0; itProd.hasNext(); i++) {
+                prodTemp = (ProductoX) itProd.next();
+
+                //Genera los datos de las filas para la columna Codigo de Producto
+                sheet.addCell(new jxl.write.Label(0, fila, prodTemp.getNombre()));
+
+                //Genera los datos de las filas para la columna Descripcion
+                sheet.addCell(new jxl.write.Label(1, fila, prodTemp.getDescripcion()));
+
+                //Genera los datos de las filas para la columna cantidad vendidas
+                sheet.addCell(new jxl.write.Number(2, fila, prodTemp.getCantVendidas()));
+
+                fila++;
+            }
+
+            //Escribimos los resultados al fichero Excel
+            workbook.write();
+            workbook.close();
+
+
+            System.out.println("Creacion del reporte finalizado.");
+        } catch (IOException ex) {
+            System.out.println("Error al crear el fichero.");
+        } catch (WriteException ex) {
+            System.out.println("Error al escribir el fichero.");
+        }
+    }
 }
--- trunk/MotoR1/daoHibernateImpl/FacturaDaoImpl.java
+++ trunk/MotoR1/daoHibernateImpl/FacturaDaoImpl.java
@@ -18,12 +18,12 @@ import util.HibernateUtil;
  */
 public class FacturaDaoImpl implements FacturaDao {
 
+    
     public void agregarFactura(Factura factura) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         try {
             session.beginTransaction();
             session.save(factura);
-            
             session.getTransaction().commit();
         } catch (Exception e) {
             System.out.println(e.toString());
@@ -63,10 +63,11 @@ public class FacturaDaoImpl implements FacturaDao {
         List facturas = null;
         try {
             session.beginTransaction();
-            Query getFacturas = session.createQuery("from Factura fact where month(fact.fecha) = :var1 and  fact.is_anulado = :isAnulada");
+            Query getFacturas = session.createQuery("from Factura fact where month(fact.fecha) = :var1 and  fact.is_anulado = :isAnulada and year(fact.fecha)= :anio");
             getFacturas.setInteger("var1", mes); // la funcion esta Deprecated hay que buscar otra
-            getFacturas.setString("isAnulada", "N");
 
+            getFacturas.setString("isAnulada", "N");
+            getFacturas.setInteger("anio", new Date().getYear() + 1900);//solo las facturas de este anio** Nota: Date().getYear() retorna 108 (en vez de 2008)
             facturas = getFacturas.list();
 
         } catch (Exception e) {
@@ -77,13 +78,14 @@ public class FacturaDaoImpl implements FacturaDao {
         return facturas;
     }
 
-        public List facturasPorAnio(int anio) {
+    public List facturasPorAnio(int anio) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         List facturas = null;
         try {
             session.beginTransaction();
             Query getFacturas = session.createQuery("from Factura fact where year(fact.fecha) = :var1 and where fact.is_anulado = :isAnulada");
             getFacturas.setInteger("var1", anio); // la funcion esta Deprecated hay que buscar otra
+
             getFacturas.setBoolean("isAnulada", false);
 
             facturas = getFacturas.list();
@@ -95,7 +97,7 @@ public class FacturaDaoImpl implements FacturaDao {
         }
         return facturas;
     }
-    
+
     public List facturasPorFecha(int anioDesde, int anioHasta, int mesDesde, int mesHasta) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         List facturas = null;
@@ -106,7 +108,7 @@ public class FacturaDaoImpl implements FacturaDao {
             getFacturas.setInteger("ahasta", anioHasta);
             getFacturas.setInteger("mdesde", mesDesde);
             getFacturas.setInteger("mhasta", mesHasta);
-             getFacturas.setString("isAnulada", "N");
+            getFacturas.setString("isAnulada", "N");
             facturas = getFacturas.list();
 
 
@@ -141,9 +143,10 @@ public class FacturaDaoImpl implements FacturaDao {
         List facturas = null;
         try {
             session.beginTransaction();
-            Query getFacturas = session.createQuery("from Factura fact where month(fact.fecha) = :var1 and fact.is_anulado = :isAnulada");
+            Query getFacturas = session.createQuery("from Factura fact where month(fact.fecha) = :var1 and fact.is_anulado = :isAnulada and year(fact.fecha) = :anio");
             getFacturas.setInteger("var1", mes);
-             getFacturas.setString("isAnulada", "Y");
+            getFacturas.setString("isAnulada", "Y");
+            getFacturas.setInteger("anio", new Date().getYear() + 1900); //solo las facturas de este anio ->Date().getYear() retorna 108 (en vez de 2008)
 
             facturas = getFacturas.list();
 
@@ -162,7 +165,7 @@ public class FacturaDaoImpl implements FacturaDao {
             session.beginTransaction();
             Query getFacturas = session.createQuery("from Factura fact where fact.id = :ncodigo");
             getFacturas.setInteger("ncodigo", codigo);
-            factura = (Factura)getFacturas.uniqueResult();
+            factura = (Factura) getFacturas.uniqueResult();
 
         } catch (Exception e) {
             System.out.println(e.toString());
@@ -171,10 +174,10 @@ public class FacturaDaoImpl implements FacturaDao {
         }
         return factura;
     }
-    
+
     public List facturasSinImpuesto(int mes) {
         Session session = HibernateUtil.getSessionFactory().openSession();
-         List facturas = null;
+        List facturas = null;
         try {
             session.beginTransaction();
             Query getFacturas = session.createQuery("from Factura fact where fact.sin_impuesto = :estado and month(fact.fecha) = :pMes");
