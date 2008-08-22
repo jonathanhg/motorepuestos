53192ef214c17d4aa505c35451341de937ef36e6
jonathanhg <jonathanhg@99eec449-2851-0410-a4d2-bb893c3c0773>
2008-08-22 13:46:41 +0000

    Agregado "Cliente de contado" como cliente default (cuando no se ingresa el nombre del cliente)

--- MotoR1/model/Factura.java
+++ MotoR1/model/Factura.java
@@ -1,6 +1,5 @@
 package model;
 
-
 import java.io.Serializable;
 import java.util.Date;
 
@@ -26,38 +25,33 @@ import javax.persistence.TemporalType;
 import org.hibernate.annotations.Type;
 
 @Entity
-@Table(name="factura")
-public class Factura implements Serializable  {
-    
-     public Factura () {
-         
-     }
+@Table(name = "factura")
+public class Factura implements Serializable {
+
+    public Factura() {
+    }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.68E95536-1830-1897-AFCA-FDD9C63E3C70]
     // </editor-fold> 
     private Integer id;
-       // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.F4443D97-F382-5B6E-18AE-4B536434DFA8]
     // </editor-fold> 
-    
-    
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="id", unique=true)
-    public Integer getId () {
+    @Column(name = "id", unique = true)
+    public Integer getId() {
         return id;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.8EF88379-55CC-7281-CFAF-0B26A818512C]
     // </editor-fold> 
-    public void setId (Integer val) {
+    public void setId(Integer val) {
         this.id = val;
     }
-    
-
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.DE037891-D01E-6A23-16AA-F2779A1D06C3]
     // </editor-fold> 
@@ -92,43 +86,41 @@ public class Factura implements Serializable  {
     // #[regen=yes,id=DCE.2D02A437-35E0-78AD-1601-DBEE9A64244B]
     // </editor-fold> 
     private boolean is_anulado;
-    
     private boolean sin_impuesto;
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.77586E61-C6F3-81AB-C3F5-46C95807EF05]
     // </editor-fold> 
-   
-
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.0833DA86-9ED1-86B0-755F-E9BB9FEF1BDC]
     // </editor-fold> 
-    public String getCliente () {
+    public String getCliente() {
         return cliente;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.AB554424-7D57-400E-B640-C7C4AD7C493F]
     // </editor-fold> 
-    public void setCliente (String val) {
-        this.cliente = val;
+    public void setCliente(String val) {
+        if (val.equals("")) {
+            this.cliente = "CLIENTE DE CONTADO";
+        } else {
+            this.cliente = val;
+        }
+
     }
-
-
-
-
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.CF837DC0-25C8-6C7E-1A77-D780A43E788C]
     // </editor-fold> 
-    public double getDescuento () {
+    public double getDescuento() {
         return descuento;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.0070D507-D480-C5A7-9386-D4271E5AC654]
     // </editor-fold> 
-    public void setDescuento (double val) {
+    public void setDescuento(double val) {
         this.descuento = val;
     }
 
@@ -136,73 +128,68 @@ public class Factura implements Serializable  {
     // #[regen=yes,regenBody=yes,id=DCE.DF30B582-7376-A399-600B-36DE9035D088]
     // </editor-fold> 
     @Temporal(TemporalType.DATE)
-    public Date getFecha () {
+    public Date getFecha() {
         return fecha;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.ED5AE0E6-3BAB-9A41-72E1-D5474777F8EE]
     // </editor-fold> 
-    public void setFecha (Date val) {
+    public void setFecha(Date val) {
         this.fecha = val;
     }
-
- 
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.E2C45C78-F6A5-A05D-C2B9-9AE8FAC1CBD2]
     // </editor-fold> 
-    @Type(type="yes_no")
-    public boolean getIs_anulado () {
+    @Type(type = "yes_no")
+    public boolean getIs_anulado() {
         return is_anulado;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.02E2935B-3CCE-B3EA-3E03-D4C931D0A100]
     // </editor-fold> 
-    public void setIs_anulado (boolean val) {
+    public void setIs_anulado(boolean val) {
         this.is_anulado = val;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.B5E73EBF-B9F5-5EBB-88F8-B5BE76E169F2]
     // </editor-fold> 
-    public double getPrecio () {
+    public double getPrecio() {
         return precio;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.6E857CF6-911A-5B33-7B78-D93E22516DC3]
     // </editor-fold> 
-    public void setPrecio (double val) {
+    public void setPrecio(double val) {
         this.precio = val;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.95752227-13EC-302F-0FF8-2C6A3CD9D068]
     // </editor-fold> 
-    public double getTotal () {
+    public double getTotal() {
         return total;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.5049EF3E-8EEF-E44C-E747-5B16F1488D29]
     // </editor-fold> 
-    public void setTotal (double val) {
+    public void setTotal(double val) {
         this.total = val;
     }
-    
     private List<FactProduct> productos;
 
     // Borra todos los productos cuando se elimina factura ,  
     //crea nuevos productos en la factura sin tener que consultarlos antes para insertarlos , 
     //se utliza Eager para que traiga los campos ya inicializados
-    @ManyToMany(cascade=CascadeType.ALL , fetch=FetchType.EAGER) 
-    @JoinTable(
-        name="FactProductos",
-        joinColumns={@JoinColumn(name="fact_id",unique=false)},
-        inverseJoinColumns= @JoinColumn(name="product_id",unique=false)
-    )
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "FactProductos",
+    joinColumns = {@JoinColumn(name = "fact_id", unique = false)},
+    inverseJoinColumns = @JoinColumn(name = "product_id", unique = false))
     public List<FactProduct> getProductos() {
         return productos;
     }
@@ -218,6 +205,5 @@ public class Factura implements Serializable  {
     public void setSin_impuesto(boolean estado) {
         this.sin_impuesto = estado;
     }
-
 }
 
